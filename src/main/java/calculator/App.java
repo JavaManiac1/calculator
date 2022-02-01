5b449ed82b05c6ac1ae98ea0713951009ee63bb9
Damian <damian_joniec@interia.pl>
2022-02-01 03:06:15 +0100

    Add Logic to Main class.

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -1,13 +1,33 @@
 package calculator;
 
+import calculator.service.EventCalculator;
+import org.apache.commons.io.IOUtils;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.List;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) throws IOException {
+        System.out.println( "-----------Slowest responses-----------" );
+
+
+        FileInputStream file = new FileInputStream(args[0]);
+//        FileInputStream file = new FileInputStream("src/main/resources/logfile.txt");
+        String data = IOUtils.toString(file, "UTF-8");
+
+        EventCalculator eventCalculator = new EventCalculator();
+
+        List<String> slowResponseList =  eventCalculator.returnSlowResponseIds(data);
+
+
+        System.out.println("Response IDs that takes more than 4ms:");
+        for(String slowRespRecord : slowResponseList){
+            System.out.println(slowRespRecord);
+        }
     }
 }
